8c29940afbce56170221be1e65ad5c98705784ea
Hayley-Woodhouse <h.woodhouse639@gmail.com>
2022-10-12 16:56:26 +0000

    updated procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn boutique_ado.wsgi:application
+web: gunicorn lakeside-prints.wsgi:application
--- lakeside_prints/settings.py
+++ lakeside_prints/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+if os.path.exists("env.py"):
+    import env #noqa
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i7nhav9w^2%0+6+r%0sesi)+vc-05%u48kar#!ia$*6+^rmrd8'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = ['lakeside_prints.herokuapp.com', 'localhost']
 
